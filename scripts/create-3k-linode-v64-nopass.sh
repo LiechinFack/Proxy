2e6544f853c96e1d295d9f9e1185b2df04732fc6
Mai Nhân Phúc <phucmnp@gmail.com>
2022-12-06 16:35:16 +0700

    update readme

--- scripts/create-3k-linode-v64-nopass.sh
+++ scripts/create-3k-linode-v64-nopass.sh
@@ -56,6 +56,9 @@ setgid 65535
 setuid 65535
 stacksize 6291456
 flush
+#auth strong
+
+#users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "\n" \
 "\n" \
