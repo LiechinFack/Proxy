ebe37a5c58ae8dbcc4b0ef8b2dd04b623f5e1eac
Mai Nhân Phúc <phucmn@vissoft.vn>
2022-12-09 01:10:25 +0700

    code ngu thi phai fix bug

--- scripts/create-3k-linode-v64-nopass.sh
+++ scripts/create-3k-linode-v64-nopass.sh
@@ -75,13 +75,6 @@ EOF
 
 upload_proxy() {
     cd $WORKDIR
-    local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -F "file=@proxy.zip" https://file.io)
-
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
-    echo "Password: ${PASS}"
 
     cp proxy.txt "$IP4".txt
 
--- scripts/create-linode-pass-v4.sh
+++ scripts/create-linode-pass-v4.sh
@@ -75,14 +75,6 @@ EOF
 
 upload_proxy() {
     cd $WORKDIR
-    local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -F "file=@proxy.zip" https://file.io)
-
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
-    echo "Password: ${PASS}"
-
     cp proxy.txt "$IP4".txt
 
     curl -X POST -F 'document=@/home/proxy-installer/'"$IP4"'.txt' -F 'chat_id=-892127097' https://api.telegram.org/bot5748050505:AAEZEtv9aSEHk5VWmWVr68jiYP9KlimddnE/sendDocument
