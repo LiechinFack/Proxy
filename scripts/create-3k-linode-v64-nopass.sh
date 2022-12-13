12933106bf3c9c8cbbfe90c0e2632e89bc0ae72c
Mai Nhân Phúc <phucmnp@gmail.com>
2022-12-13 14:39:47 +0700

    update readme

--- scripts/create-3k-linode-v64-nopass.sh
+++ scripts/create-3k-linode-v64-nopass.sh
@@ -141,7 +141,6 @@ chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
-systemctl start NetworkManager.service
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 65535
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
